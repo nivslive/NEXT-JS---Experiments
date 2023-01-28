6e451ce886045c1a59b586d5351eaa8875e0343d
Nivs <nivsoficial@gmail.com>
2023-01-28 11:25:23 -0300

    t

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json(require('../../../storage/structure.json'))
+  res.status(200).json(require('../../../storage/portfolio/structure.json'))
 }
